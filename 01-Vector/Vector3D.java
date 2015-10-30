499ad152490ba677b186361d4b4133db6217a4c2
Ade Rizki <aderizki0405@gmail.com>
2015-10-30 17:24:44 +0700

    vecto1

--- 01-Vector/Vector3D.java
+++ 01-Vector/Vector3D.java
@@ -3,15 +3,30 @@
 public class Vector3D
 {
     // instance variables - replace the example below with your own
-     double x;
-     double y;
-     double z;
-     
+    private double x;
+    private double y;
+    private double z;
+   
     public Vector3D(double x, double y, double z)
     {
        this.x=x;
        this.y=y;
        this.z=z;
+    }
+    
+     public double getX()
+    {
+       return x;
+    }
+    
+     public double getY()
+    {
+       return y;
+    }
+    
+     public double getZ()
+    {
+       return z;
     }
 
    
