503e458530fc15eea6c072d63b89b9a81ad9c273
Ade Rizki <aderizki0405@gmail.com>
2015-10-30 16:32:05 +0700

    perubahan 1

---
+++ 01-Vector/Vector3D.java
@@ -0,0 +1,19 @@
+
+
+public class Vector3D
+{
+    // instance variables - replace the example below with your own
+     double x;
+     double y;
+     double z;
+     
+    public Vector3D(double x, double y, double z)
+    {
+       this.x=x;
+       this.y=y;
+       this.z=z;
+    }
+
+   
+   
+}
